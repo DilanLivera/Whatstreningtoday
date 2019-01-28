f9090d229d4629372020418ef31b1529de0db6f0
DilanLivera <DilanLivera@users.noreply.github.com>
2019-01-28 17:00:46 +1100

    add landing page

--- app.js
+++ app.js
@@ -16,9 +16,16 @@ var client = new Twitter({
 //config app
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"))
 
 //home route
 app.get("/", (req, res) => {
+    res.render("landingpage");
+});
+
+//tweet route
+app.get("/tweet", (req, res) => {
     res.redirect("/results");
 });
 
